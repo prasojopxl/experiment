cb0802044ad8d2a8ffc6cb4b3b7369c99e00abd4
Sapto Prasojo <prasojodesigner@gmail.com>
2022-11-22 22:43:26 +0700

    register

--- migrations/20221119113905-users.js
+++ migrations/20221119113905-users.js
@@ -1,55 +1,55 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add altering commands here.
      *
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-     await queryInterface.createTable('users', 
-     { 
+    await queryInterface.createTable("users", {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
-        autoIncrement:true,
-        allowNull: false
+        autoIncrement: true,
+        allowNull: false,
       },
       name: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
       },
       email: {
         type: Sequelize.STRING,
-        allowNull:false
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       role: {
         type: Sequelize.STRING,
-        allowNull:false
+        allowNull: true,
       },
       created_at: {
-        type:Sequelize.DATE,
-        allowNull:false
+        type: Sequelize.DATE,
+        allowNull: false,
       },
       updated_at: {
-        type:Sequelize.DATE,
-        allowNull:false
-      }
-
-    }
-     );
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-     await queryInterface.dropTable('users');
-
-  }
+    await queryInterface.dropTable("users");
+  },
 };
---
+++ routes/handlers/auth/register.js
@@ -0,0 +1,37 @@
+const { User } = require("../../../models");
+const bcrypt = require("bcrypt");
+
+module.exports = async (req, res) => {
+  const { body } = req;
+
+  //validate input
+  if (!body.name || !body.email || !body.password) {
+    return res.status(400).json({
+      message: "name, email and password must be provided",
+    });
+  }
+
+  // check email has been used
+  const isEmailUsed = await User.findOne({
+    where: {
+      email: body.email,
+    },
+  });
+
+  if (isEmailUsed)
+    return res.status(400).json({
+      message: "email already to use",
+    });
+
+  const password = await bcrypt.hashSync(body.password, 10);
+
+  const user = await User.create({
+    ...body,
+    password,
+  });
+
+  return res.json({
+    id: user.id,
+    name: user.name,
+  });
+};
